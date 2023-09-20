aa137af604813494820ee3c300d0c402ebe11c3d
bhunao <bh.felix@hotmail.com>
2023-09-20 18:50:18 -0300

    add stuff

--- main.nim
+++ main.nim
@@ -16,28 +16,12 @@ type
 
 var
   frog: Entity 
-  tongue: Entity
-  bait: Entity
 
 proc initGame =
   frog.texture = loadTexture("resources/frog_sit.png")
   frog.source = Rectangle(x: 0, y: 0, width: frog.texture.width.toFloat, height: frog.texture.height.toFloat)
   frog.dest = Rectangle(x: screenWidth/2, y: 0, width: frog.texture.width.toFloat, height: frog.texture.height.toFloat)
   frog.origin = Vector2(x: frog.texture.width.toFloat/2, y:0)
-
-  tongue.texture = loadTexture("resources/tongue_middle.png")
-  tongue.source = Rectangle(x: 0, y: 0, width: tongue.texture.width.toFloat, height: tongue.texture.height.toFloat)
-  tongue.dest = Rectangle(x: screenWidth/2 + 2.0, y: frog.texture.height.toFloat, width: tongue.texture.width.toFloat, height: 15)
-  tongue.origin = Vector2(x: tongue.texture.width.toFloat/2, y:0)
-
-  bait.texture = loadTexture("resources/tongue_end.png")
-  bait.source = Rectangle(x: 0, y: 0, width: bait.texture.width.toFloat, height: bait.texture.height.toFloat)
-  bait.dest = Rectangle(x: screenWidth/2 + 2.0, y: frog.texture.height.toFloat, width: bait.texture.width.toFloat, height: bait.texture.height.toFloat)
-  bait.origin = Vector2(x: bait.texture.width.toFloat/2, y:0)
-
-proc move(entity: var Entity, x, y: float) =
-  entity.dest.x += x
-  entity.dest.y += y
 
 proc draw*(entity: var Entity) =
   drawTexture(
@@ -50,19 +34,13 @@ proc draw*(entity: var Entity) =
   )
 
 proc updateGame =
-  var diff = (getMouseY() - bait.dest.y) / 15
-  bait.move(0.0, diff)
-  bait.dest.y = max(bait.dest.y, 150)
-
-  tongue.dest.height = bait.dest.y - tongue.dest.y
+  discard
 
 proc drawGame =
   beginDrawing()
   clearBackground(RayWhite)
 
   draw frog
-  draw tongue
-  draw bait
 
   endDrawing()
 
