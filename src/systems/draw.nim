c18758df05c6b7a4b0286a6fc3fe40abd4aa6e4d
bhunao <bh.felix@hotmail.com>
2023-10-02 22:20:52 -0300

    one day i will add sprites

--- src/systems/draw.nim
+++ src/systems/draw.nim
@@ -1,7 +1,9 @@
 import raylib
 import ../components
+import ../componentutils
 import ../yeacs
 import std / lenientops
+import ../sprites
 
 
 proc drawFrame*(world: var World) =
@@ -11,5 +13,11 @@ proc drawFrame*(world: var World) =
   for (pos, circle) in world.foreach (Position, Circle):
     drawCircle(pos.x.int32, pos.y.int32, circle.r, circle.c)
 
+  # for (pos, spr) in world.foreach (Position, Sprite):
+  #   var 
+  #     position = Vector2(x: pos.x, y: pos.y)
+  #     txture = texturas[SPACESHIP]
+  #   drawTexture(txture, position, spr.rotation, spr.scale, spr.color)
+# void DrawTextureEx(Texture2D texture, Vector2 position, float rotation, float scale, Color tint);  // Draw a Texture2D with extended parameters
 
   endDrawing()
