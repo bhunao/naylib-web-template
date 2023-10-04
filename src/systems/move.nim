1aa715db7f6eb8c2bc7a4637a6230af13166c3a9
bhunao <bh.felix@hotmail.com>
2023-10-03 23:20:12 -0300

    add: cartesiam keyboard movement

--- src/systems/move.nim
+++ src/systems/move.nim
@@ -8,6 +8,10 @@ proc moveSystem*(world: var World, maxWidth, maxHeight: int) =
     pos.x += vel.speed * cos degToRad vel.angle
     pos.y += vel.speed * sin degToRad vel.angle
 
+  for (pos, vel) in world.foreach (Position, CartesianVelocity):
+    pos.x += vel.x
+    pos.y += vel.y
+
     when defined(debugMove):
       debugEcho "Moved " & ecs.inspect(entity) & " from ", oldPosition, " to ", po
 
