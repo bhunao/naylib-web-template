969fffb24c9f0e4e666883cf089ced5a4c4a54d1
bhunao <bh.felix@hotmail.com>
2023-10-03 23:19:41 -0300

    add: draw squares

--- src/systems/draw.nim
+++ src/systems/draw.nim
@@ -1,8 +1,8 @@
 import raylib
+import std/lenientops
+
 import ../components
-import ../componentutils
 import ../yeacs
-import std / lenientops
 import ../sprites
 
 
@@ -13,11 +13,7 @@ proc drawFrame*(world: var World) =
   for (pos, circle) in world.foreach (Position, Circle):
     drawCircle(pos.x.int32, pos.y.int32, circle.r, circle.c)
 
-  # for (pos, spr) in world.foreach (Position, Sprite):
-  #   var 
-  #     position = Vector2(x: pos.x, y: pos.y)
-  #     txture = texturas[SPACESHIP]
-  #   drawTexture(txture, position, spr.rotation, spr.scale, spr.color)
-# void DrawTextureEx(Texture2D texture, Vector2 position, float rotation, float scale, Color tint);  // Draw a Texture2D with extended parameters
+  for (pos, sqr) in world.foreach (Position, Square):
+    drawRectangle(pos.x.int32, pos.y.int32, sqr.w.int32, sqr.h.int32, sqr.c)
 
   endDrawing()
